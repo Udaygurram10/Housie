a300316f7bd02ea3ee1809ef90a6921f54a4552f
Uday Gurram <148441798+Udaygurram10@users.noreply.github.com>
2025-01-14 11:32:15 +0530

    Google Analytics Update 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Layout } from './components/Layout';
+import { useGoogleAnalytics } from './hooks/useGoogleAnalytics';
+
+const AppContent = () => {
+  useGoogleAnalytics();
+  return <Layout />;
+};
 
 export const App = () => (
   <BrowserRouter>
-    <Layout />
+    <AppContent />
   </BrowserRouter>
 );
